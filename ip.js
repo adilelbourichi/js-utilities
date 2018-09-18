c142b46e7c2b4035e91418025db4d11ff229e28f
Adil EL Bourichi <adilelbourichi@gmail.com>
2018-09-18 20:54:47 +0800

    updated script

--- ip.js
+++ ip.js
@@ -8,19 +8,30 @@ if(ipParts[ipParts.length - 1] == false) {
 	ipParts = ipParts.slice(0, -1);
 }
 
+var maxIpAddress = ipAddress;
+if(ipParts[ipParts.length-1].length === 1) {
+	maxIpAddress += '99';
+}
+if(ipParts[ipParts.length-1].length === 2) {
+	maxIpAddress += '9';
+}
+
 if(ipParts.length === 3) {
 	for (var i = 1; i <= 255; i++) {
-		var searchAddress = i + '.' + ipAddress;
-		ranges.push([searchAddress, searchAddress]);
+		var minRangeAddress = i + '.' + ipAddress;
+		var maxRangeAddress = i + '.' + maxIpAddress;
+		ranges.push([minRangeAddress, maxRangeAddress]);
 	}
 }
 else if(ipParts.length === 2) {
 	for (var i = 1; i <= 255; i++) {
+		var minRangeAddress = i + '.' + ipAddress;
+		var maxRangeAddress = i + '.' + maxIpAddress;
+		ranges.push([minRangeAddress  + '.0', maxRangeAddress + '.255']);
 		for (var j = 1; j<= 255; j++) {
-			var searchAddress = i + '.' + j + '.' + ipAddress;
-			ranges.push([searchAddress, searchAddress]);
-			var searchAddress = i + '.' + ipAddress + '.' + j;
-			ranges.push([searchAddress, searchAddress]);
+			var minRangeAddress = i + '.' + j + '.' + ipAddress;
+			var maxRangeAddress = i + '.' + j + '.' + maxIpAddress;
+			ranges.push([minRangeAddress, maxRangeAddress]);
 		}
 	}
 }
